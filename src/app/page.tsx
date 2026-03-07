5c5f25122592f061e953da6cecf98e7f5f53149d
MukilanFabriplay <mukilan@fabriplay.com>
2026-03-07 12:46:02 +0530

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Hello Docker UI Test 🚀</h1>
-      <p>If you see this, Next.js build works.And changes  that reflects</p>
+      <p>If you see this, Next.js build works.And changes</p>
     </div>
   );
 }
